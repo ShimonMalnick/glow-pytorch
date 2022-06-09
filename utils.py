6c492f1722b3e1e04e28fa74db5356ecae85c0da
Shimon Malnick <shimonmalnick@gmail.com>
2022-06-09 23:59:36 +0300

    removed defaults from argparse arguments

--- utils.py
+++ utils.py
@@ -9,12 +9,12 @@ from torch.utils.data import DataLoader
 
 def get_args() -> EasyDict:
     parser = ArgumentParser(description="Glow trainer", formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--batch", default=16, type=int, help="batch size")
-    parser.add_argument("--iter", default=200000, type=int, help="maximum iterations")
+    parser.add_argument("--batch", help="batch size")
+    parser.add_argument("--iter", help="maximum iterations")
     parser.add_argument(
-        "--n_flow", default=32, type=int, help="number of flows in each block"
+        "--n_flow", help="number of flows in each block"
     )
-    parser.add_argument("--n_block", default=4, type=int, help="number of blocks")
+    parser.add_argument("--n_block", help="number of blocks")
     parser.add_argument(
         "--no_lu",
         action="store_true",
@@ -24,23 +24,23 @@ def get_args() -> EasyDict:
     parser.add_argument(
         "--affine", action="store_true", help="use affine coupling instead of additive", default=False
     )
-    parser.add_argument("--n_bits", default=5, type=int, help="number of bits")
-    parser.add_argument("--lr", default=1e-4, type=float, help="learning rate")
-    parser.add_argument("--img_size", default=64, type=int, help="image size")
-    parser.add_argument("--temp", default=0.7, type=float, help="temperature of sampling")
-    parser.add_argument("--n_sample", default=20, type=int, help="number of samples")
-    parser.add_argument("--ckpt_path", default='', help='Path to checkpoint for model')
-    parser.add_argument("--opt_path", default='', help='Path to checkpoint for optimizer')
+    parser.add_argument("--n_bits", help="number of bits")
+    parser.add_argument("--lr", help="learning rate")
+    parser.add_argument("--img_size", help="image size")
+    parser.add_argument("--temp", help="temperature of sampling")
+    parser.add_argument("--n_sample", help="number of samples")
+    parser.add_argument("--ckpt_path", help='Path to checkpoint for model')
+    parser.add_argument("--opt_path", help='Path to checkpoint for optimizer')
     celeba_path = '/home/yandex/AMNLP2021/malnick/datasets/celebA/celeba'
     ffhq_path = '/home/yandex/AMNLP2021/malnick/datasets/ffhq-128'
-    parser.add_argument("--path", metavar="PATH", default=celeba_path, type=str, help="Path to image directory")
-    parser.add_argument('--eval', action='store_true', default=False, help='Use for evaluating '
+    parser.add_argument("--path", metavar="PATH", help="Path to image directory")
+    parser.add_argument('--eval', action='store_true', help='Use for evaluating '
                                                                            'a model')
-    parser.add_argument('--sample_name', default='', help='Name of sample size in case of evaluation')
-    parser.add_argument('--exp_name', default='', help='Name experiment for saving dirs')
-    parser.add_argument('--num_workers', default=8, type=int, help='Number of worker threads for dataloader')
-    parser.add_argument('--config', default='', help='Name of json config file (optional) cmd will be overriden by file option')
-    parser.add_argument('--devices', default=4, type=int, help='number of gpu devices to use')
+    parser.add_argument('--sample_name', help='Name of sample size in case of evaluation')
+    parser.add_argument('--exp_name', help='Name experiment for saving dirs')
+    parser.add_argument('--num_workers', help='Number of worker threads for dataloader')
+    parser.add_argument('--config', help='Name of json config file (optional) cmd will be overriden by file option')
+    parser.add_argument('--devices', help='number of gpu devices to use')
 
     args = parser.parse_args()
     out_dict = vars(args)
