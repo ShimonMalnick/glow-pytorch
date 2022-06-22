0df5897a6800672dfff84ed1f823c47e142fdace
Shimon Malnick <shimonmalnick@gmail.com>
2022-06-22 11:10:50 +0300

    Added interpolation video ability

--- analyze_model.py
+++ analyze_model.py
@@ -1,10 +1,15 @@
 from typing import List
-
+from PIL import Image, ImageFont, ImageDraw
+import imageio
+import matplotlib.pyplot as plt
 import torch
 from utils import get_args
 from torchvision import utils
+from torchvision.transforms import ToTensor, Resize, Compose
 from model import Glow
 from train import calc_z_shapes
+import json
+import numpy as np
 
 
 def load_model(args, device):
@@ -25,9 +30,9 @@ def get_z(args, device) -> List:
     return z_sample
 
 
-def sample_from_model(z_samples, model, out_name='samples.png'):
+def sample_from_model(z_samples, model, out_name='samples.png', reconstruct=True):
     with torch.no_grad():
-        cur_tensors = model.reverse(z_samples).cpu()
+        cur_tensors = model.reverse(z_samples, reconstruct=reconstruct).cpu()
         utils.save_image(
             cur_tensors,
             out_name,
@@ -47,10 +52,53 @@ def sample_different_temp(args, device):
         sample_from_model(cur_z, model, out_name=f'samples_{round(temp, 2)}.png')
 
 
+def make_interpolation_video(args, im1_path, im2_path, model, device, alpha_steps=41, alpha_max=2.0, base_path='outputs/interpolation'):
+    steps = torch.linspace(0, alpha_max, alpha_steps)
+    im1, im2 = Image.open(im1_path), Image.open(im2_path)
+    image_transform = Compose([ToTensor(), Resize((args.img_size, args.img_size))])
+    tens_1, tens_2 = image_transform(im1).unsqueeze(0), image_transform(im2).unsqueeze(0)
+    tens_1 = tens_1.to(device) - 0.5  # normalization as the input is expected to be in [-0.5, 0.5]
+
+    tens_2 = tens_2.to(device) - 0.5  # normalization as the input is expected to be in [-0.5, 0.5]
+    with torch.no_grad():
+        _, _, z_1 = model(tens_1)
+        _, _, z_2 = model(tens_2)
+
+    deltas_z = [sub_z_2 - sub_z_1 for sub_z_1, sub_z_2 in zip(z_1, z_2)]
+    with open(f"outputs/interpolation/info.txt", "w") as f:
+        f.write(f"images paths: im1: {im1_path}\nim2: {im2_path}\n")
+        f.write(f'alpha_steps: {alpha_steps}\nalpha_max: {alpha_max}\n')
+    for i in range(alpha_steps):
+        cur_z = [sub_z_1 + sub_delta_z * steps[i] for sub_z_1, sub_delta_z in zip(z_1, deltas_z)]
+        cur_image = model.reverse(cur_z, reconstruct=True).cpu()
+        utils.save_image(
+            cur_image,
+            f'outputs/interpolation/images/alpha_{round(steps[i].item(), 2)}.png',
+            normalize=True,
+            range=(-0.5, 0.5),
+        )
+    create_video_with_labels(base_path + '/video.mp4', [base_path + f'/images/alpha_{round(s.item(), 2)}.png' for s in
+                                                                  steps], [f'alpha={round(s.item(), 2)}' for s in steps])
+
+
+def create_video_with_labels(out_path, paths, labels, fps=2, codec='libx264', bitrate='16M'):
+    video = imageio.get_writer(out_path, mode='I', fps=fps, codec=codec, bitrate=bitrate)
+    images = [Image.open(path) for path in paths]
+    font = ImageFont.truetype('f1.ttf', 16)
+    for i in range(len(paths)):
+        cur_draw = ImageDraw.Draw(images[i])
+        cur_draw.text((70, 0), labels[i], font=font, fill=(255, 0, 0))
+        video.append_data(np.asarray(images[i]))
+
+    video.close()
+
+
 def main():
     args = get_args()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    sample_different_temp(args, device)
+    model = load_model(args, device)
+    images_base = "outputs/interpolation"
+    make_interpolation_video(args, images_base + "/im1.png", images_base + "/im2.png", model, device)
 
 
 if __name__ == '__main__':
