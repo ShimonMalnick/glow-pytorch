0be9edf41755053104320a04be2af940ece7732c
Shimon Malnick <shimonmalnick@gmail.com>
2022-07-07 14:57:34 +0300

    fixed stats split bug

--- data_stats.py
+++ data_stats.py
@@ -14,12 +14,13 @@ from functools import reduce
 
 
 def get_celeba_stats(split='train', out_dir='outputs/celeba_stats'):
+    file_name = os.path.join(out_dir, f'identities_{split}.pt')
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    if not os.path.exists(os.path.join(out_dir, 'identities.pt')):
+    if not os.path.exists(file_name):
 
         begin = time()
-        ds = get_dataset('/home/yandex/AMNLP2021/malnick/datasets/celebA', 128, split=split)
+        ds = get_dataset('/home/yandex/AMNLP2021/malnick/datasets/celebA', 128, data_split=split)
         dl = DataLoader(ds, batch_size=256, shuffle=False, num_workers=16)
         cur = time()
         print("building dataset took: ", round(cur - begin, 2), " seconds")
@@ -30,11 +31,12 @@ def get_celeba_stats(split='train', out_dir='outputs/celeba_stats'):
         all_identities = torch.cat(all_identities)
         print("all_ids_shape: ", all_identities.shape)
         print("iterating through dataset took: ", round(time() - cur, 2), " seconds")
-        torch.save(all_identities, os.path.join(out_dir, 'identities.pt'))
+        torch.save(all_identities, file_name)
     else:
-        all_identities = torch.load(os.path.join(out_dir, 'identities.pt'))
+        all_identities = torch.load(file_name)
     num_identities = 10177
-    hist_tensor = torch.bincount(all_identities, minlength=num_identities)[1:].float()
+    hist_tensor = torch.bincount(all_identities, minlength=num_identities)[1:].float()  # sliced to remove 0 since
+    # identities are labeled {1,2, ..., num_identities}
     print(hist_tensor[:50])
     non_zero_elements = hist_tensor[hist_tensor.nonzero()]
     data = {'split': split,
@@ -51,23 +53,23 @@ def get_celeba_stats(split='train', out_dir='outputs/celeba_stats'):
             "non_zero_median": round(torch.median(non_zero_elements).item(), 2)}
     print("min id: ", torch.min(all_identities))
     print("max id: ", torch.max(all_identities))
-    with open(os.path.join(out_dir, 'stats.json'), 'w') as f:
+    with open(os.path.join(out_dir, f'stats_{split}.json'), 'w') as f:
         json.dump(data, f, indent=4)
     plt.figure(figsize=(20, 10))
     plt.bar(np.arange(num_identities), hist_tensor.numpy())
-    plt.savefig(os.path.join(out_dir, 'hist.png'))
+    plt.savefig(os.path.join(out_dir, f'hist_{split}.png'))
 
 
-def save_images_chosen_identities(ids: List[int], save_dir: str):
+def save_images_chosen_identities(ids: List[int], save_dir: str, split: str='train'):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    ds = get_dataset('/home/yandex/AMNLP2021/malnick/datasets/celebA', 128, split='train')
-    all_identities = torch.load(os.path.join('outputs/celeba_stats', 'identities.pt'))
+    ds = get_dataset('/home/yandex/AMNLP2021/malnick/datasets/celebA', 128, data_split=split)
+    all_identities = torch.load(os.path.join('outputs/celeba_stats', f'identities_{split}.pt'))
     for identity in ids:
-        cur_dir = os.path.join(save_dir, str(identity), "images")  # images added just to use ImageFolder dataset
+        cur_dir = os.path.join(save_dir, str(identity), split, "images")  # images added just to use ImageFolder dataset
         os.makedirs(cur_dir, exist_ok=True)
         ids_tensors = (all_identities == identity).nonzero(as_tuple=True)[0]
-        print(ids_tensors.shape)
+        print(f"for split {split} and identity {identity} got: {ids_tensors.shape} images")
         for i, id_tensor in enumerate(ids_tensors):
             cur_img, cur_label = ds[id_tensor]
             assert cur_label.item() == identity
@@ -80,7 +82,8 @@ def parse_line(line):
     return Counter({i: int(line[i]) for i in range(len(line)) if line[i] == '1'})
 
 
-def get_celeba_attributes_stats(attr_file_path: str='/home/yandex/AMNLP2021/malnick/datasets/celebA/celeba/list_attr_celeba.txt'):
+def get_celeba_attributes_stats(attr_file_path: str='/home/yandex/AMNLP2021/malnick/datasets/celebA/celeba/list_attr_celeba.txt',
+                                save_stats_path='outputs/celeba_stats/attributes_stats.json'):
     start_parse = time()
     with open(attr_file_path, 'r') as f:
         num_images = int(f.readline().strip())
@@ -97,9 +100,14 @@ def get_celeba_attributes_stats(attr_file_path: str='/home/yandex/AMNLP2021/maln
             attributes_dict[attributes_map[k]] = reduced[k]
     end_parse = time()
     print("Parsing all attributes took: ", round(end_parse - start_parse, 2), " seconds")
-    create_horizontal_bar_plot(attributes_dict, 'outputs/celeba_attributes_stats.png',
+    if save_stats_path:
+        with open(save_stats_path, 'w') as f:
+            json.dump(attributes_dict, f, indent=4)
+    save_time = time()
+    print("saving took ", round(save_time - end_parse, 2), " seconds")
+    create_horizontal_bar_plot(attributes_dict, 'outputs/celeba_stats/celeba_attributes_stats.png',
                                title='CelebA Binary Atrributes Count (out of {} images)'.format(num_images))
-    print("plotting took ", round(time() - end_parse, 2), " seconds")
+    print("plotting took ", round(time() - save_time, 2), " seconds")
 
 
 if __name__ == '__main__':
