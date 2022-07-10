5704f3f59c481259ea62da7179390d92c10260b2
Shimon Malnick <shimonmalnick@gmail.com>
2022-07-10 15:51:45 +0300

    fixed a bug in saving checkpoint in forget.py

--- forget.py
+++ forget.py
@@ -1,11 +1,10 @@
 import os
-
 from model import Glow
 import torch
-from utils import get_args, load_model, quantize_image, sample_data, make_exp_dir, save_dict_as_json, \
+from utils import get_args, load_model, quantize_image, make_exp_dir, save_dict_as_json, \
     save_model_optimizer, CELEBA_ROOT
 from torch.utils.data import DataLoader
-from typing import Iterator, List, Callable, Dict
+from typing import Iterator, Dict
 from torchvision.transforms import Compose, RandomHorizontalFlip, ToTensor, Resize
 from train import calc_loss
 from datasets import CelebAPartial, ForgetSampler
@@ -21,6 +20,7 @@ def get_partial_dataset(transform, **kwargs) -> CelebAPartial:
     identities/images. See documnetaion of CelebAPArtial for more details.
     """
     ds = CelebAPartial(root=CELEBA_ROOT, transform=transform, target_type="identity", **kwargs)
+    print("len of dataset: ", len(ds))
     return ds
 
 
@@ -43,7 +43,7 @@ def get_data_iterator(ds: CelebAPartial, batch_size, num_workers=16) -> Iterator
 
 
 def forget(args, remember_iter: Iterator, forget_iter: Iterator, model: Glow, device, forget_optimizer,
-           regular_optimizer):
+           remember_optimizer):
     n_bins = 2 ** args.n_bits
     for i in range(args.iter):
         if (i + 1) % args.forget_every == 0:
@@ -57,7 +57,7 @@ def forget(args, remember_iter: Iterator, forget_iter: Iterator, model: Glow, de
             loss_sign = 1.0
             loss_name = "regular"
             cur_batch, _ = next(remember_iter)
-            optimizer = regular_optimizer
+            optimizer = remember_optimizer
         cur_batch = cur_batch.to(device)
         log_p, logdet, _ = model(cur_batch + torch.rand_like(cur_batch) / n_bins)
 
@@ -81,17 +81,26 @@ def forget(args, remember_iter: Iterator, forget_iter: Iterator, model: Glow, de
         )
 
         if (i + 1) % args.save_every == 0:
-            torch.save(
-                model.state_dict(), f'experiments/{args.exp_name}/checkpoints/model_{str(i + 1).zfill(6)}.pt'
-            )
-            torch.save(
-                forget_optimizer.state_dict(),
-                f'experiments/{args.exp_name}/checkpoints/forget_optim_{str(i + 1).zfill(6)}.pt'
-            )
-            torch.save(
-                regular_optimizer.state_dict(),
-                f'experiments/{args.exp_name}/checkpoints/regular_optim_{str(i + 1).zfill(6)}.pt'
-            )
+            save_forget_checkpoint(args, forget_optimizer, i, model, remember_optimizer)
+    save_forget_checkpoint(args, forget_optimizer, 0, model, remember_optimizer, last=True)
+
+
+def save_forget_checkpoint(args, forget_optimizer, iter_num, model, remember_optimizer, last=False):
+    if last:
+        model_id = "last"
+    else:
+        model_id = str(iter_num + 1).zfill(6)
+    torch.save(
+        model.state_dict(), f'experiments/forget/{args.exp_name}/checkpoints/model_{model_id}.pt'
+    )
+    torch.save(
+        forget_optimizer.state_dict(),
+        f'experiments/forget/{args.exp_name}/checkpoints/forget_optim_{model_id}.pt'
+    )
+    torch.save(
+        remember_optimizer.state_dict(),
+        f'experiments/forget/{args.exp_name}/checkpoints/regular_optim_{model_id}.pt'
+    )
 
 
 def args2dset_params(args, ds_type) -> Dict:
@@ -106,16 +115,20 @@ def args2dset_params(args, ds_type) -> Dict:
            "exclude_identities": None,
            "include_only_images": None,
            "exclude_images": None}
+    if 'data_split' in args and args.data_split in ['train', 'all']:
+        out['split'] = args.data_split
     if ds_type == 'forget':
         if args.forget_identity:
             out["include_only_identities"] = [args.forget_identity]
-        if args.forget_images:
-            out["include_only_images"] = os.listdir(args.forget_images)
+        elif args.forget_images:
+            assert args.forget_size, "must specify forget_size if forget_images is specified"
+            out["include_only_images"] = os.listdir(args.forget_images)[:args.forget_size]
     if ds_type == 'remember':
         if args.forget_identity:
             out["exclude_identities"] = [args.forget_identity]
-        if args.forget_images:
-            out["exclude_images"] = os.listdir(args.forget_images)
+        elif args.forget_images:
+            assert args.forget_size, "must specify forget_size if forget_images is specified"
+            out["exclude_images"] = os.listdir(args.forget_images)[:args.forget_size]
 
     return out
 
@@ -130,7 +143,7 @@ def args2data_iter(args, ds_type, transform) -> Iterator:
     """
     ds_params = args2dset_params(args, ds_type)
     ds = get_partial_dataset(transform=transform, **ds_params)
-    return get_data_iterator(ds, args.batch_size)
+    return get_data_iterator(ds, args.batch, num_workers=args.num_workers)
 
 
 def forget_baseline(forget_iter: Iterator, args, model, device, optimizer):
@@ -159,15 +172,17 @@ def forget_baseline(forget_iter: Iterator, args, model, device, optimizer):
             f"Iter: {i + 1} Loss: {loss.item():.5f}; logP: {log_p.item():.5f}; logdet: {log_det.item():.5f};")
 
         if (i + 1) % args.save_every == 0:
-            save_model_optimizer(args, i, model, optimizer)
+            save_model_optimizer(args, i, model, optimizer, save_prefix='experiments/forget')
+    save_model_optimizer(args, 0, model, optimizer, save_prefix='experiments/forget', last=True)
 
 
 def main():
     args = get_args(forget=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    args.exp_name = make_exp_dir(args.exp_name)
+    args.exp_name = make_exp_dir(args.exp_name, exist_ok=False, forget=True)
     print(args)
-    save_dict_as_json(args, f'experiments/{args.exp_name}/args.json')
+    args.FORGET_THRESH = FORGET_THRESH
+    save_dict_as_json(args, f'experiments/forget/{args.exp_name}/args.json')
     wandb.init(project="Glow-Forget", entity="malnick", name=args.exp_name, config=args)
     model: torch.nn.DataParallel = load_model(args, device, training=True)
     transform = Compose([Resize((args.img_size, args.img_size)),
